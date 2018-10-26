cc9e09b73ac1b96cf3f0610434ea4dbfef2242ec
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-10-26 16:46:20 -0700

    add flexbox to display listings by zipcode

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactDataSheet from 'react-datasheet';
+// import ReactDataSheet from 'react-datasheet';
 import axios from 'axios';
+
+import ZaL from './ZipAndListings';
 
 class App extends Component {
   state = {
     selectedCity: '',
     regions: [],
-    data: ''
+    data: false
   }
 
   selectCity = async (city) => {
-    const data = await axios.post(`/api/cityname`, { "name": city });
+    const data = await axios.post(`/api/listings`, { "name": city });
     this.setState({ selectedCity: city, data: data.data });
   }
 
   render() {
     const cities = ['San Francisco', 'Los Angeles', 'San Diego', 'Nashville', 'Boston', 'Philadephia', 'Key West', 'Portland', 'San Antonio', 'Sedona', 'Austin', 'Chicago', 'Puerto Rico']
+    const listings = this.state.data && this.state.data.map(zip => <ZaL props={zip} />);
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -24,8 +28,8 @@ class App extends Component {
             {cities.map(city => <button onClick={() => this.selectCity(city)}>{city}</button>)}
           </div>
           <h3>{this.state.selectedCity}</h3>
-          <p>{this.state.data && this.state.data[0].name}</p>
         </header>
+          <div>{listings}</div>
       </div>
     );
   }
---
+++ client/src/components/ZipAndListings.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ZipAndListings = props => {
+  const { zip, top_listings } = props.props;
+  const listingElement = top_listings.map(x => <Listings props={x[0].listing} />)
+  console.log('the zip is', zip);
+  return ( <div>
+    <h3>{zip}</h3>
+    <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row'}}>{listingElement}</div>  
+  </div>
+   );
+}
+
+const Listings = props => {
+  const {city, property_type, title, annual_revenue_potential} = props.props;
+  return ( <span style={{ margin: '4rem', border: '1px white', width: '7rem', height: '7rem'}}>
+    <h5>{title}</h5>
+    <p>{`${property_type} located in ${city}, $ ${annual_revenue_potential}`}</p>
+  </span> );
+}
+ 
+
+ 
+export default ZipAndListings;
